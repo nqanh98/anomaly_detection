23d2743199c641fa9217292f8662b317c70819ae
Takuma Nozawa <tnozawa@cuire.local>
2021-08-22 13:10:10 +0900

    clustering with scaled data and index

--- display.py
+++ display.py
@@ -4,14 +4,16 @@ import matplotlib.cm as cm
 
 import numpy as np
 
-def plot_2d_scatters_for_clusters(clusters, pred):
+def plot_2d_scatters_for_clusters(clusters):
     # センターカラーの取得
+    n_clusters = len(clusters)
+    centers = np.stack([np.uint8(cluster.mean(axis=0)) for cluster in clusters])    
+    c_min = centers.min()
+    c_max = centers.max()
     colors = []
-    c_min = pred.cluster_centers_.min()
-    c_max = pred.cluster_centers_.max()
-    for n in range(pred.n_clusters):
-        #color = cm.viridis(pred.cluster_centers_[n][0] / 255)
-        color = cm.viridis((pred.cluster_centers_[n][0] - c_min) / (c_max - c_min))
+    for n in range(n_clusters):
+        #color = cm.viridis(centers[n][0] / 255)
+        color = cm.viridis((centers[n][0] - c_min) / (c_max - c_min))
         colors.append(color)
     # プロット
     fig = plt.figure(figsize=(6,6),facecolor="w")
@@ -22,17 +24,18 @@ def plot_2d_scatters_for_clusters(clusters, pred):
     # 表示範囲の設定
     ax.set_xlim(0, 255)
     ax.set_ylim(-1, 1)
-    for n in range(pred.n_clusters):
+    for n in range(n_clusters):
         x = clusters[n].reshape(-1)
         y = np.zeros(len(x))
         ax.plot(x, y, "o", color=colors[n], ms=4, mew=0.5)
     plt.show()
-
-def plot_3d_scatters_for_clusters(clusters, pred):
+    
+def plot_3d_scatters_for_clusters(clusters):
     # センターカラーの取得
+    n_clusters = len(clusters)
     colors = []
-    for n in range(pred.n_clusters):
-        color = pred.cluster_centers_[n][:3] / 255
+    for n in range(n_clusters):
+        color = clusters[n].mean(axis=0) / 255
         colors.append(color)
     # プロット
     fig = plt.figure(figsize=(10,6),facecolor="w")
@@ -45,6 +48,6 @@ def plot_3d_scatters_for_clusters(clusters, pred):
     ax.set_xlim(0, 255)
     ax.set_ylim(0, 255)
     ax.set_zlim(0, 255)
-    for n in range(pred.n_clusters):
+    for n in range(n_clusters):
         ax.plot(clusters[n][:,0], clusters[n][:,1], clusters[n][:,2], "o", color=colors[n], ms=4, mew=0.5)
     plt.show()
