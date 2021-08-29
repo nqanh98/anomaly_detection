d3ae1c8d04b1f37dfcbbd3851a06ce3c5b061a5f
Takuma Nozawa <tnozawa@cuire.local>
2021-08-29 13:02:59 +0900

    functions are moved to class

--- anomaly_detection.py
+++ anomaly_detection.py
@@ -1,20 +1,40 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from scipy import stats
 from sklearn.mixture import GaussianMixture
 
 temperature_classes = ["High", "Medium", "Low", "Exception"]
 
-def get_gmm(data):
-    gmm = GaussianMixture(
-        n_components=len(temperature_classes),
-        covariance_type='spherical'
-    ).fit(data)
-    return gmm
+class AnoGMM:
+    def __init__(self, data):
+        self.model = self.get_gmm_model(data)
+        self.index2class = self.get_index2class()
+        
+    def get_gmm_model(self, data):
+        model = GaussianMixture(
+            n_components=len(temperature_classes),
+            covariance_type='spherical'
+        ).fit(data)
+        return model
 
-def get_index2class(model):
-    index2class = {
-        idx:c for idx, c
-        in zip(np.argsort(model.means_[:, -1])[::-1],temperature_classes)
-    }
-    return index2class
+    def get_index2class(self):
+        index2class = {
+            idx:c for idx, c
+            in zip(np.argsort(self.model.means_[:, -1])[::-1],temperature_classes)
+        }
+        return index2class
 
+    def display(self):
+        fig = plt.figure(figsize=(12,4),facecolor="w")
+        ax1 = fig.add_subplot(1,1,1)
+        #x = np.linspace(-6, 2, 300)
+        x = np.linspace(0, 255, 300)
+        gd = []
+        for idx, c in self.index2class.items():
+            gd = stats.norm.pdf(x, self.model.means_[idx, -1], np.sqrt(self.model.covariances_[idx]))
+            plt.plot(x, self.model.weights_[idx] * gd, label=c)
+        plt.legend()
+        plt.show()
 
+    def predict(self,data):
+        return self.model.predict(data)
--- clustering.py
+++ clustering.py
@@ -2,16 +2,26 @@
 import numpy as np
 from sklearn.cluster import KMeans
 
-def get_kmeans(inputs, n_clusters=8):
-    model = KMeans(n_clusters=n_clusters).fit(inputs)
-    return model
+class TemperatureClusters:
+    def __init__(self, data, n_clusters=8):
+        self.n_clusters = n_clusters
+        self.model = self.get_kmeans_model(data)
+        self.labels = self.model.labels_
+        self.centers = self.model.cluster_centers_
+        
+    def get_kmeans_model(self, data):
+        model = KMeans(
+            n_clusters=self.n_clusters
+        ).fit(data)
+        return model
 
-def get_clusters_data(model, data):
-    clusters = []
-    assert data.shape[0] == model.labels_.shape[0]
-    for n in range(model.n_clusters):
-        cluster = np.array(
-            [ d for l, d in zip(model.labels_,data ) if l==n ]
-        )
-        clusters.append(cluster)
-    return clusters
+    def get_clusters_data(self, data):
+        clusters = []
+        assert data.shape[0] == self.model.labels_.shape[0]
+        for n in range(self.n_clusters):
+            cluster = np.array(
+                [ d for l, d in zip(self.model.labels_,data ) if l==n ]
+            )
+            clusters.append(cluster)
+        return clusters
+
