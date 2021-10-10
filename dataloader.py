9fe229b11098fdf8a3ed0046d38b7c204cb9de58
afnozawa <t.nozawa@afterfit.co.jp>
2021-10-10 18:57:32 +0900

    log-scale detection

--- dataloader.py
+++ dataloader.py
@@ -27,25 +27,51 @@ class ThermalData:
         temperature = {
             k: v.reshape(-1,v.shape[2]) for k, v in thermal_img_files.items()
         }
-        all_temperature = np.concatenate([*temperature.values()])
         self.temperature = temperature
-        self.all_temperature = all_temperature        
+        self.all_temperature = np.concatenate([*temperature.values()])
         # -- 1d scaled flatten thermal data --
         sscaler = preprocessing.StandardScaler()
         if scale_type == "individual":
             scaled_temperature = {
-                k: sscaler.fit_transform(v.reshape(-1,v.shape[2])) for k, v in thermal_img_files.items() # scale individualy
+                k: sscaler.fit_transform(v.reshape(-1,3)) for k, v in thermal_img_files.items() # scale individualy
             }
         elif scale_type == "all":
             sscaler.fit(all_temperature)
             scaled_temperature = {
-                k: sscaler.transform(v.reshape(-1,v.shape[2])) for k, v in thermal_img_files.items() # scaled by all temperature
+                k: sscaler.transform(v.reshape(-1,3)) for k, v in thermal_img_files.items() # scaled by all temperature
             }
         else:
             print("not supported scale type:",scale_type)
-        scaled_all_temperature = np.concatenate([*scaled_temperature.values()])            
         self.scaled_temperature = scaled_temperature
-        self.scaled_all_temperature = scaled_all_temperature
+        self.scaled_all_temperature = np.concatenate([*scaled_temperature.values()])
+        # -- 1d masked flatter thermal data --
+        masked_temperature = {
+            k: temperature[k][(v>-2) & (v<2)].reshape(-1,3) for k, v in scaled_temperature.items()
+        }
+        self.masked_temperature = masked_temperature
+        self.masked_all_temperature = np.concatenate([*masked_temperature.values()])        
+        # -- 1d scaled masked flatter thermal data --
+        scaled_masked_temperature = {
+            k: sscaler.fit_transform(v) for k, v in masked_temperature.items()
+        }
+        self.scaled_masked_temperature = scaled_masked_temperature
+        self.scaled_masked_all_temperature = np.concatenate([*scaled_masked_temperature.values()])
+        # -- 1d transformed flatten thermal data --
+        pscaler = preprocessing.PowerTransformer(standardize=True)
+        if scale_type == "individual":
+            transformed_temperature = {
+                k: pscaler.fit_transform(v.reshape(-1,3)) for k, v in thermal_img_files.items() # scale individualy
+            }
+        elif scale_type == "all":
+            pscaler.fit(all_temperature)
+            transformed_temperature = {
+                k: sscaler.transform(v.reshape(-1,3)) for k, v in thermal_img_files.items() # scaled by all temperature
+            }
+        else:
+            print("not supported scale type:",scale_type)
+        self.transformed_temperature = transformed_temperature
+        self.transformed_all_temperature = np.concatenate([*transformed_temperature.values()])
+        
 
     def get_data_with_index(self, data):
         data_with_index = []
